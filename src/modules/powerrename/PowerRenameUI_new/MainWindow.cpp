0891f758c23178b9f3a858d6781d8a19d7a74c83
Stefan Markovic <stefan@janeasystems.com>
2021-08-25 23:06:03 +0200

    Init 1

---
+++ src/modules/powerrename/PowerRenameUI_new/MainWindow.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "MainWindow.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::PowerRenameUI_new::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        Button().Content(box_value(L"Clicked"));
+    }
+}
